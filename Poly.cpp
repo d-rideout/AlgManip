768c8eb2a3232a931fa7662c25223f0c91329aa8
David Rideout <ufahamu@pm.me>
2022-05-25 03:48:48 -0700

    construct polynomial from array

--- Poly.cpp
+++ Poly.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+
 #include "Poly.h"
 using namespace std;
 
 Poly::Poly() {
-  //  cout << "Poly constructor" << endl;
+  cout << "Poly bare constructor" << endl;
+  cout << "DELETE ME?" << endl;
+}
+
+Poly::Poly(int nterms, int p[]) {
+  cout << "Poly array constructor" << endl;
+  this->n = nterms;
+  //for (int i=0; i<nterms; ++i) this->p.push_back(p[i]); // memcpy()??
+  this->p.resize(2*nterms);
+  copy(p, p+2*nterms, this->p.begin());
+  for (unsigned int i=0; i<this->p.size(); ++i) cout << this->p.at(i) << ' ';
+  cout << endl;
 }
 
 Poly::~Poly() {
   cout << "(virtual) Poly destructor" << endl;
+  cout << this->n << " terms" << endl;
+  for (const int& i : this->p) cout << i << "  ";
+  cout << endl;
 }
 
 // Poly& Poly::operator=(const Poly &other) {
@@ -17,6 +32,6 @@ Poly::~Poly() {
 
 // constructor from another object, to make a copy
 Poly::Poly(const Poly &other) {
-
+  cout << "Poly copy constructor" << endl;
 }
 
--- Poly.h
+++ Poly.h
@@ -1,14 +1,21 @@
 #ifndef POLY_H_
 #define POLY_H_
 
+#include <vector>
+using namespace std;
+
 class Poly {
-public:
+ private:
+	int n=1;
+	vector<int> p;
+ public:
 	Poly();
+	Poly(int nterms, int p[]);
+	// See also stackoverflow.com/questions/4118025/brace-enclosed-initializer-list-constructor
+	// en.cppreference.com/w/cpp/language/list_initialization (25may022)
 	virtual ~Poly();
 	//Poly& operator=(const Poly &other);
 	Poly(const Poly &other);
-
-	//int x=37;
 };
 
 #endif /* POLY_H_ */
