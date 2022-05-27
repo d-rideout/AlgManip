63a6e5f5b11803d7241cf77c2767badf102f679d
David Rideout <ufahamu@pm.me>
2022-05-26 21:21:33 -0700

    * check type of input to poly constructor * allow 0 coefficients

--- poly.py
+++ poly.py
@@ -1,9 +1,13 @@
 class poly:
   # store as list of 2-tuples (coef, exp) for starters
   def __init__(s, lt):
+#     print('creating poly with', lt, 'of type', type(lt), type(lt[0]))
+    print('poly:', lt)
+#     if not type(lt[0]) == "<class 'tuple'>": die('please construct with list of tuples')
+    if not isinstance(lt[0],tuple): die('please construct with list of tuples')
     s.p = lt
 
-  def __del__(s): print('destroying poly instance with', len(s.p), 'terms...')
+#   def __del__(s): print('destroying poly instance with', len(s.p), 'terms...')
     
   def __str__(s):
     retval = ''
@@ -12,10 +16,10 @@ class poly:
       c = t[0]
       if c<-1: cs = str(c)
       elif c==-1: cs = '-'
-      elif c==0: die('zero coeff')
+#       elif c==0: die('zero coeff')
       elif c==1: cs = '+'
       else: cs = f'+{c}'
-      
+      if not c: continue
 #       if t[0]<0: plus=''
 #       else: plus='+'
 #       retval += sp+plus+f'{t[0]} q^{t[1]}'
@@ -28,11 +32,12 @@ class poly:
     pd = {} # key exponent val coeff
     for tl in s.p:
       for tr in o.p:
+#         print(f'multiplying {tl} with {tr}')
         e = tl[1]+tr[1]
-        print(e)
+#         print(e)
         if e in pd: pd[e] += tl[0]*tr[0]
         else: pd[e] = tl[0]*tr[0]
-    print(sorted(pd))
+#     print(sorted(pd))
     p = []
     for e in sorted(pd): p.append((pd[e],e))
     return poly(p)
