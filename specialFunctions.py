cd8be0f722ed3c5cf722a64bbe7d95c48477e53b
David Rideout <ufahamu@pm.me>
2022-06-15 12:37:21 -0700

    implement my own q-Pochhammer function

--- specialFunctions.py
+++ specialFunctions.py
@@ -1,4 +1,24 @@
-# Taken from github.com/ectomancer/pure_python/blob/main/q-analog.ipynb on 14jun022
+def qpoch(q, n=None):
+  '''(q,q)_n = [n]_q! (1-q)^n = (1-q)(1-q^2)...(1-q^n)
+  no n ==> (q,q)_infinity aka Euler function'''
+  if n==None:
+    m = 0
+    eps = 1e-9
+    prevVal = qpoch(q,m)
+    val = qpoch(q,m+1) # Though this recursive definition is surely inefficient!
+    while abs(prevVal-val) > eps:
+      m += 1
+      prevVal = val
+      val = qpoch(q,m+1)
+    print('converged at n=', m, 'to within eps=', eps)
+    return val
+  if n==0: return 1 # [0]_q! (1-q)^0 = 1
+#   if n==1: return 1-q # Is this a good idea?
+  return (1-q**n)*qpoch(q,n-1)
+
+  
+# Below taken from github.com/ectomancer/pure_python/blob/main/q-analog.ipynb on 14jun022
+# ---------------------------------------------------------------------------------------
 
 from typing import List, TypeVar
 
@@ -16,9 +36,10 @@ def q_poch(a: Numeric, q: Numeric, n: int=None) -> Numeric:
     """
     #Special case of q-binomial thereom.
     if n is None:
-        sum = EMPTY_SUM
-        for n in range(30):
-            sum += MINUS_ONE**n*q**(n*(n - 1)/2)/q_poch(q, q, n)*a**n
+        sum = 1 # EMPTY_SUM
+        for n in range(1,23): sum += MINUS_ONE**n*q**(n*(n - 1)/2)/q_poch(q, q, n)*a**n
+        # was to 30
+        # Above is hitting a numeric overflow or some sort of instability near q=1
         return sum
     if not n:
         return 1
