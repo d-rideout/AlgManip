9720228f2106c1b075b385708e447da6009a03a0
David Rideout <ufahamu@pm.me>
2022-06-16 23:38:35 -0700

    generate full recursion causet

--- recursion.py
+++ recursion.py
@@ -1,6 +1,8 @@
 from os.path import exists
 import AlgManip.util as u
 
+debug = False
+
 class var:
   def __init__(sf, name, mins):
     'mins is tuple of min value for each slot'
@@ -12,37 +14,57 @@ class var:
 
   
 class relation:
-  def __init__(sf, lhs, rhs):
+  def __init__(sf, lhs, rhs, cf=lambda ix: True):
     '''lhs: var
     ns = num slots
     rhs: tuple of (1+ns)-tuples (var, (slot, offset), (slot, offset), ...)
-    first slot is 0'''
-    print('lhs:', lhs)
+    first slot is 0
+    cf = constraintFunction to check if indices valid'''
     sf.lhs = lhs
-    print('rhs:')
+    sf.rhs = rhs
+    sf.cf = cf
+    if debug: print('lhs:', lhs)
+    if debug: print('rhs:')
     sf.lhsMins = [0]*len(lhs.mins)
     for t in rhs:
-      print(' t = ', t)
+      if debug: print(' t = ', t)
       for v in t[1:]:
-        print('  v = ', v)
+        if debug: print('  v = ', v)
         if sf.lhsMins[v[0]]+v[1] < 0: sf.lhsMins[v[0]] = -v[1]
     # Be sure mins are at least as large as required for the variables
     for s in range(len(lhs.mins)):
       if sf.lhsMins[s] < lhs.mins[s]: sf.lhsMins[s] = lhs.mins[s]
 
+  def validLHS(sf, ix): return sf.cf(ix) # invalidLHS?
+
 
 def recursionCauset(fn, rl, max):
+  'writes graphviz file of recursion causet'
   if exists(fn): print(fn, 'exists, overwriting')
   rt = fn[:-4]
   fp = open(fn, 'w')
   fp.write(f'# dot -Tpdf -o {rt}.pdf {fn}\ndigraph "{rt}" ')
   fp.write('{\nrankdir=BT; concentrate=true; node[shape=plaintext];\n')
   for r in rl:
-    print(r.lhs, r.lhsMins)
-    i = r.lhsMins[:]
+    if debug: print(r.lhs, r.lhsMins)
+    ix = r.lhsMins[:]
     while True:
-      print(i)
-      if u.incList(i, r.lhsMins, max): break
-
+      if not r.validLHS(ix):
+        if u.incList(ix, r.lhsMins, max): break # having second thoughts about loop...
+        continue
+      if debug: print('index=', ix)
+      if debug: fp.write('lhs='+str(r.lhs)+str(ix))
+      lhs = str(r.lhs)+str(ix[0])
+      for i in ix[1:]:
+        lhs += '_'+str(i)
+      for t in r.rhs:
+#         fp.write('\n# '+str(t)+'\n')
+        fp.write(f' {t[0].name}')
+        for j in range(1,len(t)):
+          if j>1: fp.write('_')
+          fp.write(str(ix[t[j][0]]+t[j][1]))
+        fp.write(f'->{lhs};')
+      fp.write(f'\n')
+      if u.incList(ix, r.lhsMins, max): break
   fp.write('}\n')
   fp.close()
