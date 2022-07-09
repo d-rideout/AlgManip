1fb7e620458d0f7f74295ca33f59dee7bb1302e8
David Rideout <ufahamu@pm.me>
2022-07-09 14:48:15 -0700

    attempt to understand dir() & .__dict__

--- __init__.py
+++ __init__.py
@@ -1,16 +1,35 @@
 print('Importing AlgManip python package module')
 
-if 1:
-  # from os import getcwd
+if False:
   import os
   from datetime import date
   cwd = os.getcwd()
   date = date.today()
 
-  print(f'(from {cwd} on {date})')
+  print(f'(from {cwd} on {date})', end='\n\n')
 
-a  print('module init file =', __file__)
+  print('package module init file =', __file__)
   # https://www.delftstack.com/howto/python/get-directory-from-path-in-python
-  print('module dir =', os.path.dirname(__file__))
+  print('package module directory =', os.path.dirname(__file__), end='\n\n')
 
-  print('module name =', __name__)
+  print('module __name__ (from perspective of importing program) =', __name__, end='\n\n')
+
+  print('dir() (names in current scope):', dir(), end='\n\n')
+#   print('dir(__name__):', dir(__name__), end='\n\n')
+#   print(__name__.__class__)
+# __name__ is just a string!
+
+#   print('__dict__:', __dict__, end='\n\n')
+#   print('__name__.__dict__:', __name__.__dict__, end='\n\n')
+# neither the current scope nor the string object __name__ have a __dict__ attribute
+
+  class junk: pass
+  print("empty class's __dict__ attribute:", junk.__dict__) # classes have __dict__attributes
+  print("empty class instance's __dict__ attribute:", junk().__dict__) # classes have __dict__attributes
+
+#   print(os.__dict__) # as do modules (but it is enormous for os)
+
+  def junkf(): pass
+  print("empty function's __dict__ attribute:", junkf.__dict__)
+
+  exit()
