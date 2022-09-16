1424fac529a3a716b68e76de2d1e91732b17b6cd
David Rideout <ufahamu@pm.me>
2022-09-16 12:30:18 -0700

    think in terms of random graphs

--- graph.py
+++ graph.py
@@ -1,30 +1,31 @@
-'Fast DAG (directed acyclic graph) module' # include all graphs?
-# 'Fast digraph module' (drop the acyclic assumption?)
+# 'Fast DAG (directed acyclic graph) module' # include all graphs?
+'Fast graph module' # (drop the acyclic assumption?)
 
 def i2bit(i,j): return j*(j-1)//2+i  # Map from pair of indices i<j to bit
 
 def popcount(n): return bin(n).count('1')
 # www.valuedlessons.com/2009/01/popcount-in-python-with-benchmarks.html
-# First comment suggests that this is not completely terrible??
+# First comment suggests that this is not completely terrible??!
 # Python 3.10 has more native popcount?
 # Just use this for now and profile (12jul022)
 
 
-# class digraph: # distinguish this from an ordinary graph?
-# And really it is a DAG
-class dag:
-  def __init__(s, n, g):
+class graph:
+  dag = False
+  def __init__(s, n, gr):
     # Do I want to pass n each time, or make it global?
     s.n = n
-    s.g = g
+    s.gr = gr
   def __str__(s): # cf pypi.org/project/diGraph ?
     rv = ''
-    sp = ''
+    spc = ''
+    if dag: rn = '<'
+    else: rn = '-'
     for j in range(1,s.n):
       for i in range(j):
         if s.g & 1<<i2bit(i,j):
-          rv += sp + f'{i}<{j}'
-          sp = ' '
+          rv += sp + f'{i}{rn}{j}'
+          spc = ' '
     return rv
 
 
