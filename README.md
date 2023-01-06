bcf743edd485f94989a48c4cf74cd4a54dd3eacd
David Rideout <ufahamu@pm.me>
2023-01-05 21:24:22 -0800

    * Maintain log of calling programs * Remove C++ style entries from .gitignore

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 library to perform algebraic manipulations
 
 (and a playground for thinking about machine intelligence)
+
+File | Role
+---- | ----
+`Poly_test.cpp` | created by Eclipse, may be removed in future
--- __init__.py
+++ __init__.py
@@ -1,43 +1,67 @@
 print('Importing AlgManip python package module')
 
-import sys # Anybody can access this via AlgManip.sys
 
+# import sys # Anybody can access this via AlgManip.sys
 # print(sys.path)
-sys.path.insert(1,'AlgManip')
+# sys.path.insert(1,'AlgManip')
+# Commented above line to understand all this, see docs.python.org/3/reference/import.html @ 5.3 Searching (5jan023)
 # print(sys.path)
-# My local imports are not package imports when called from other directories
+
+# This allows files within AlgManip to import other files in AlgManip directly.
+# But is this silly?
+# What happens if someone does not put AlgManip in a directory which is in
+# their python path?
+# Oh, I think this question is answered in the next sentence?
+# Please understand all this! The code below may be helpful for this. (29nov022)
+
+# My local imports are not package imports when called from other directories.
 # But am I duplicating modules this way?  Probably.  How do people manage this?
+#   print('module __name__ (from perspective of importing program) =', __name__, end='\n\n')
+#   # __name__ is just a string!
 
-if False:
+
+if True: # Maintain log of calling programs
   import os
   from datetime import date
-  cwd = os.getcwd()
-  date = date.today()
+  main = os.environ['_']
+  cwd = os.getcwd() + '/'
+  date = str(date.today())
 
-  print(f'(from {cwd} on {date})', end='\n\n')
+  print(f'(from {main} running in {cwd} on {date})', end='\n\n')
 
-  print('package module init file =', __file__)
-  # https://www.delftstack.com/howto/python/get-directory-from-path-in-python
-  print('package module directory =', os.path.dirname(__file__), end='\n\n')
+  packageDir = os.path.dirname(__file__)
+  logfile = open(packageDir+'/.usageLog', 'a')
+#   logfile.write('\t'.join(('Date\t', 'Program\t\t\t', 'cwd'))+os.linesep)
+  logfile.write('\t'.join((date, main, cwd))+os.linesep)
+  logfile.close()
 
-  print('module __name__ (from perspective of importing program) =', __name__, end='\n\n')
+#   print('package module init file =', __file__)
+#   # https://www.delftstack.com/howto/python/get-directory-from-path-in-python
+#   print('package module directory =', os.path.dirname(__file__), end='\n\n')
 
-  print('dir() (names in current scope):', dir(), end='\n\n')
-#   print('dir(__name__):', dir(__name__), end='\n\n')
-#   print(__name__.__class__)
-# __name__ is just a string!
+#   print('dir() (names in current scope):', dir(), end='\n\n')
+
+#   print(os.name)
+#   print('cwd:', os.getcwd())
+#   print(os.listdir())
+#   print(*os.environ) (no idea why .keys() fails to list keys!)
+#   print(os.environ['_'])
+
+#   exit()
 
 #   print('__dict__:', __dict__, end='\n\n')
 #   print('__name__.__dict__:', __name__.__dict__, end='\n\n')
-# neither the current scope nor the string object __name__ have a __dict__ attribute
+# neither the current scope nor the string object __name__ have a __dict__ attribute:
+# global scope never has __dict__ it seems.  i.e. no one can ever use __dict__ as a local name.
+# And __name__ is a string class.  dir() returns a class's methods, but classes implemented in C (such as the built-in class str) do not have a __dict__ attribute.
+# https://stackoverflow.com/questions/9502183/python-why-is-dict-attribute-not-in-built-in-class-instances (29nov022)
 
-  class junk: pass
-  print("empty class's __dict__ attribute:", junk.__dict__) # classes have __dict__attributes
-  print("empty class instance's __dict__ attribute:", junk().__dict__) # classes have __dict__attributes
-
-#   print(os.__dict__) # as do modules (but it is enormous for os)
-
-  def junkf(): pass
-  print("empty function's __dict__ attribute:", junkf.__dict__)
-
-  exit()
+# User defined classes, class instances and functions have __dict__ attribute:
+# class junk: pass
+#   print("empty class's __dict__ attribute:", junk.__dict__) # classes have __dict__attributes
+#   print("empty class instance's __dict__ attribute:", junk().__dict__) # classes have __dict__attributes
+#
+# #   print(os.__dict__) # as do modules (but it is enormous for os)
+#
+#   def junkf(): pass
+#   print("empty function's __dict__ attribute:", junkf.__dict__)
