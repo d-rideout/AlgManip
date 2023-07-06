bd92bc9fdef4b6a05cb4defc5f3d3eb90f3fbeb6
David Rideout <ufahamu@pm.me>
2023-07-05 18:54:04 -0700

    * store causets in yaml file * provide sequence of causets via generator function

--- causet.py
+++ causet.py
@@ -1,7 +1,42 @@
+import yaml
+from os.path import dirname
 import AlgManip.graph as gm
+
+# Always read poscau database upon import?
+# with open(dirname(__file__) + '/poscau.yaml', 'r') as fp:
+fp = open(dirname(__file__) + '/poscau.yaml', 'r')
+# poscau = yaml.safe_load_all(fp)
+poscau = yaml.safe_load(fp)
+#for doc in poscau: print(doc)
+  #print(len(poscau))
+
+
+
 gm.Graph.size = 's'
 gm.Graph.nl = True
 gm.Graph.verb = False
+
+print(poscau)
+
+def junk(): print('junk')
+
+def nextCauset():
+  # what to call this??  iterCauset??  but I don't want to imply completeness!
+  #for doc in poscau: print(type(doc), doc)
+  # Do I want to use multiple docs?
+  print('hi')
+  # global poscau
+  print('nextCauset():', poscau)
+  for c in poscau:
+    print('c=', c)
+    if isinstance(c, dict):
+      print('dict')
+      n = c['n']
+      sbs = c['sbs']
+    else:
+      print(c[0])
+      yield gm.Graph(n,c[1])
+
 
 ch1 = gm.Graph(1)
 
@@ -19,6 +54,6 @@ V.sbs = 'tr'
 # Can provide generator function which yields sequence of small causets
 # Can store details in a tuple of tuples for each n
 
-Proposal:
-* store poscau in a yaml file
-* store structure of poscau in igraph
+# Proposal:
+# * store poscau in a yaml file
+# * store structure of poscau in igraph
