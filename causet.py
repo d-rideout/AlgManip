e9c692e24e08b09e0f6664488c5801145399b429
David Rideout <ufahamu@pm.me>
2023-07-05 20:59:50 -0700

    poscau.yaml spec1

--- causet.py
+++ causet.py
@@ -6,18 +6,21 @@ debug = False
 
 # Always read poscau database upon import?
 # with open(dirname(__file__) + '/poscau.yaml', 'r') as fp:
+# nicer not to have to enclose everything in the with scope
 fp = open(dirname(__file__) + '/poscau.yaml', 'r')
-# poscau = yaml.safe_load_all(fp)
-poscau = yaml.safe_load(fp)
-#for doc in poscau: print(doc)
-  #print(len(poscau))
-# print('poscau =', poscau)
+poscau = yaml.safe_load_all(fp)
+# poscau = yaml.safe_load(fp)
+
+# Read keyword definitions
+kw = next(poscau)
+# print(kw.keys()) just store set of keywords? 5jul023
+# print(next(poscau))
+# for k in next(poscau): kw[k] =
 
 # Graph settings for small causets
 gm.Graph.size = 's'
 gm.Graph.nl = True
 gm.Graph.verb = False
-
 
 def nextCauset():
   # what to call this??  iterCauset??  but I don't want to imply completeness!
@@ -26,30 +29,31 @@ def nextCauset():
   # Do I want to use multiple docs?
   # global poscau -- seems not necessary
   if debug: print('nextCauset():', poscau)
-  for c in poscau:
+  for c in next(poscau):
     if debug: print('c=', c)
-    if isinstance(c, dict):
-      if debug: print('dict')
-      n = c['n']
-#       sbs = c['sbs']
+    if isinstance(c, int):
+      n = c
+      print('\nn =', n)
+    elif isinstance(c, dict):
+      for k in c:
+        if not k in kw: print('unrecognized key:', k)
     else:
 #       print(c[0]) # name
       yield c[0], gm.Graph(n,c[1], c[2])
+  print('...')
 
-
-ch1 = gm.Graph(1)
-
-ch2 = gm.Graph(2,1)
-ch2.sbs = 'tcr'
-ach2 = gm.Graph(2)
-ach2.sbs = 'tcr'
-
-# We just provide links for now
-ch3 = gm.Graph(3,5)
-ch3.sbs = 'tr'
-V = gm.Graph(3,5)
-V.sbs = 'tr'
+# Do we want these?
+# ch1 = gm.Graph(1)
+#
+# ch2 = gm.Graph(2,1)
+# ch2.sbs = 'tcr'
+# ach2 = gm.Graph(2)
+# ach2.sbs = 'tcr'
+#
+# ch3 = gm.Graph(3,5)
+# ch3.sbs = 'tr'
+# V = gm.Graph(3,5)
+# V.sbs = 'tr'
 
 # Proposal:
-# * store poscau in a yaml file
-# * store structure of poscau in igraph
+# * store structure of poscau in igraph?
