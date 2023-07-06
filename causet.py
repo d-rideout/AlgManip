e3c7d7fcc1c48ce379a8f66777d670072cf2f671
David Rideout <ufahamu@pm.me>
2023-07-06 11:18:08 -0700

    * store children of causets * clean causet.py

--- causet.py
+++ causet.py
@@ -12,35 +12,32 @@ class abbrevDict(dict):
     else: return super().__getitem__(i)
 
 # Always read poscau database upon import?
-# with open(dirname(__file__) + '/poscau.yaml', 'r') as fp:
-# nicer not to have to enclose everything in the with scope
 fp = open(dirname(__file__) + '/poscau.yaml', 'r')
 poscau = yaml.safe_load_all(fp)
-# poscau = yaml.safe_load(fp)
 
 # Read keyword definitions
-kw = next(poscau)
-# print(kw.keys()) just store set of keywords? 5jul023
-# print(next(poscau))
-# for k in next(poscau): kw[k] =
+kw = next(poscau) # print(kw.keys()) just store set of keywords? 5jul023
 
 # Read causet abbreviations
 causetName = abbrevDict(next(poscau))
-getCauset = {}
+getCauset = {} # store causet info such that it can be retreived from abbrev
+# Note that causetName will not choke on invalid abbreviations.
+# getCauset would have to be used to check for typos, but it is not populated
+# with children!
+# Note that (the 3rd) 'next(poscau)' is just a python list I think. 6jul023
 
 # Graph settings for small causets
 gm.Graph.size = 's'
 gm.Graph.nl = True
 gm.Graph.verb = False
 
-nc = None
+nc = None # num causets at each stage
 def nextCauset():
   # what to call this??  iterCauset??  but I don't want to imply completeness!
   # poscauTraverse??  pcTrav? 5jul023
-  #for doc in poscau: print(type(doc), doc)
-  # Do I want to use multiple docs?
+  'yields abbrev, Graph instance, parents, children'
   # global poscau -- seems not necessary
-  global nc # I think because I define it here before using it
+  global nc # because I define it here before using it
   if debug: print('nextCauset():', poscau)
   for c in next(poscau):
     if debug: print('c=', c)
@@ -54,22 +51,20 @@ def nextCauset():
         if not k in kw: print('unrecognized key:', k)
       print('dict-based causets not implemented yet')
     else:
-      c += [None]*(4-len(c)) # this is not super-safe: can forget elts in middle
+      c += [None]*(5-len(c)) # this is not super-safe: can forget elts in middle
       #       if len(c)==4: yield c[0], gm.Graph(n,c[1], c[2]), c[3]
       #       else: yield c[0], gm.Graph(n,c[1], c[2]), None
       getCauset[c[0]] = (causetName[c[0]], n, c[1], c[2], c[3])
       nc += 1
-      yield c[0], gm.Graph(n,c[1], c[2]), c[3]
+      yield c[0], gm.Graph(n,c[1], c[2]), c[3], c[4]
   print('...')
 
 # Do we want these?
 # ch1 = gm.Graph(1)
-#
 # ch2 = gm.Graph(2,1)
 # ch2.sbs = 'tcr'
 # ach2 = gm.Graph(2)
 # ach2.sbs = 'tcr'
-#
 # ch3 = gm.Graph(3,5)
 # ch3.sbs = 'tr'
 # V = gm.Graph(3,5)
@@ -77,3 +72,6 @@ def nextCauset():
 
 # Proposal:
 # * store structure of poscau in igraph?
+#   I am not sure that this is worthwhile at the moment.
+#   I fear it will add unnecessary confusion.  Better to just write my own code
+#   for now. 6jul023
